984f988f0ff36c68695553ad5eb9576ea446bbe5
rajaneesh-n <nannamrajaneesh@gmail.com>
2025-12-13 19:57:57 +1100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA5E-S8AE_bJ6nnKr6edrteY5i8Y9yzgT0",
+  authDomain: "mfg-rajaneesh.firebaseapp.com",
+  databaseURL: "https://mfg-rajaneesh-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "mfg-rajaneesh",
+  storageBucket: "mfg-rajaneesh.firebasestorage.app",
+  messagingSenderId: "796044019068",
+  appId: "1:796044019068:web:2cff2d8636c93e6a6510f9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);

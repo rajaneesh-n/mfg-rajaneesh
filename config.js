4e356ff6e62ec2bf46a7c7ec78c2048c9caad1d0
rajaneesh-n <nannamrajaneesh@gmail.com>
2025-12-13 21:44:42 +1100

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
-// Firebase v8 (compat) configuration
 var firebaseConfig = {
   apiKey: "AIzaSyA5E-S8AE_bJ6nnKr6edrteY5i8Y9yzgT0",
   authDomain: "mfg-rajaneesh.firebaseapp.com",
@@ -6,11 +5,8 @@ var firebaseConfig = {
   projectId: "mfg-rajaneesh",
   storageBucket: "mfg-rajaneesh.appspot.com",
   messagingSenderId: "796044019068",
-  appId: "1:796044019068:web:2cff2d8636c93e6a6510f9"
+  appId: "1:796044019068:web:2cff2d863c93e6a6510f9"
 };
 
-// Initialize Firebase
+// Initialize Firebase (V8)
 firebase.initializeApp(firebaseConfig);
-
-// Realtime Database reference
-var db = firebase.database();

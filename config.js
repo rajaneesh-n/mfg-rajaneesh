d4cee541dd03485f9a22282766ffe74d0942638b
rajaneesh-n <nannamrajaneesh@gmail.com>
2025-12-13 21:57:54 +1100

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,30 @@
 var firebaseConfig = {
-  apiKey: "AIzaSyA5E-S8AE_bJ6nnKr6edrteY5i8Y9yzgT0",
+  apiKey: "AIzaSyA5E-S8AE_bJ6nnKroedrteY5I8Y9yzgI0",
   authDomain: "mfg-rajaneesh.firebaseapp.com",
   databaseURL: "https://mfg-rajaneesh-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "mfg-rajaneesh",
   storageBucket: "mfg-rajaneesh.appspot.com",
   messagingSenderId: "796044019068",
-  appId: "1:796044019068:web:2cff2d863c93e6a6510f9"
+  appId: "1:796044019068:web:2cff28d63c93e6a6510f9"
 };
 
-// Initialize Firebase (V8)
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+
+/* =========================
+   SCANNER CONFIG (ADD THIS)
+   ========================= */
+
+const SCANNERS = {
+  WMS: [
+    "3245",
+    "2699",
+    "3669",
+    "3456"
+  ],
+  MUSHINY: [
+    "M101",
+    "M102",
+    "M103"
+  ]
+};

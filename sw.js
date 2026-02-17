0db026006358fa4ab922d5ad509f6ea0e0c7a26d
rajaneesh-n <nannamrajaneesh@gmail.com>
2026-02-17 11:31:14 +1100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = "scanner-pwa-v1";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./take.html",
+  "./return.html",
+  "./admin.html",
+  "./style.css",
+  "./config.js",
+  "./manifest.json",
+  "./assets/logo.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : null)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      const networkFetch = fetch(event.request)
+        .then((res) => {
+          const copy = res.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+          return res;
+        })
+        .catch(() => cached);
+
+      return cached || networkFetch;
+    })
+  );
+});
